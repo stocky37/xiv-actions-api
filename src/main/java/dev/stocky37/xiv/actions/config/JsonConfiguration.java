f932de459a181b3d52a133293be6f47dffb8e211
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-10-23 03:06:41 +0100

    Add extra categorisation info to jobs

--- src/main/java/dev/stocky37/xiv/actions/config/JsonConfiguration.java
+++ src/main/java/dev/stocky37/xiv/actions/config/JsonConfiguration.java
@@ -2,6 +2,7 @@ package dev.stocky37.xiv.actions.config;
 
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import io.quarkus.jackson.ObjectMapperCustomizer;
 import javax.inject.Singleton;
 
@@ -10,5 +11,6 @@ public final class JsonConfiguration implements ObjectMapperCustomizer {
 
 	public void customize(ObjectMapper mapper) {
 		mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+		mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
 	}
 }
