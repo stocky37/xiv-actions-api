dd3370c96dd9927ea9af6261229d6702f55a1145
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-02-05 02:58:13 +0000

    refactor: move rotation builder to its own class

--- src/main/java/dev/stocky37/xiv/actions/core/RotationService.java
+++ src/main/java/dev/stocky37/xiv/actions/core/RotationService.java
@@ -4,10 +4,8 @@ import com.ibm.asyncutil.util.Either;
 import dev.stocky37.xiv.actions.data.Action;
 import dev.stocky37.xiv.actions.data.Item;
 import dev.stocky37.xiv.actions.data.Rotation;
-import dev.stocky37.xiv.actions.data.RotationAction;
 import dev.stocky37.xiv.actions.data.RotationInput;
-import java.time.Duration;
-import java.util.ArrayList;
+import dev.stocky37.xiv.actions.json.RotationBuilder;
 import java.util.List;
 import java.util.Optional;
 import javax.enterprise.context.ApplicationScoped;
@@ -15,8 +13,6 @@ import javax.inject.Inject;
 
 @ApplicationScoped
 public class RotationService {
-
-	public static final Duration OGCD_DELAY = Duration.ofMillis(700);
 	private final ActionService actionService;
 	private final ItemService itemService;
 
@@ -30,7 +26,7 @@ public class RotationService {
 	}
 
 	public Rotation buildRotation(RotationInput input) {
-		final var actions = input.actions().stream()
+		final List<Either<Action, Item>> actions = input.actions().stream()
 			.map(id -> {
 				if(id.startsWith("i")) {
 					return itemService.findById(id.substring(1));
@@ -40,46 +36,12 @@ public class RotationService {
 			})
 			.filter(Optional::isPresent)
 			.map(Optional::get)
+			.map(opt -> (opt instanceof Action)
+				? Either.<Action, Item>left((Action) opt) : Either.<Action, Item>right((Item) opt))
 			.toList();
-		return new Rotation(buildTimeline(actions));
-	}
 
-	public List<RotationAction> buildTimeline(List<? extends Record> actions) {
-		final List<RotationAction> rotation = new ArrayList<>(actions.size());
-
-		Duration nextGcd = Duration.ZERO;
-		Duration nextOGcd = nextGcd.plus(OGCD_DELAY);
-
-		int gcdCount = 1;
-		for(final var action : actions) {
-			final Either<Action, Item> either = (action instanceof Action)
-				? Either.left((Action) action) : Either.right((Item) action);
-
-			if(either.fold(Action::onGCD, Item::onGCD)) {
-				rotation.add(new RotationAction(
-					either.left(),
-					either.right(),
-					nextGcd,
-					Optional.of(gcdCount++)
-				));
-
-				nextOGcd = nextGcd.plus(OGCD_DELAY);
-				nextGcd = nextGcd.plus(either.fold(Action::recast, Item::recast));
-
-			} else {
-				rotation.add(new RotationAction(
-					either.left(),
-					either.right(),
-					nextOGcd,
-					Optional.empty()
-				));
-				
-				nextOGcd = nextOGcd.plus(OGCD_DELAY);
-				if(nextOGcd.compareTo(nextGcd) > 0) {
-					nextGcd = nextOGcd;
-				}
-			}
-		}
-		return rotation;
+		return new RotationBuilder(actions).build();
 	}
 }
+
+
--- src/main/java/dev/stocky37/xiv/actions/data/RotationAction.java
+++ src/main/java/dev/stocky37/xiv/actions/data/RotationAction.java
@@ -2,13 +2,15 @@ package dev.stocky37.xiv.actions.data;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.Duration;
+import java.util.List;
 import java.util.Optional;
 
 public record RotationAction(
 	Optional<Action> action,
 	Optional<Item> item,
 	@JsonFormat(pattern = "MILLIS") Duration timestamp,
-	Optional<Integer> gcdNumber
+	Optional<Integer> gcdNumber,
+	List<Effect> effects
 ) {
 
 }
