223d677c7b06846cc978bb839228ae5430b43607
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-02-27 15:47:13 +0000

    feat: add delay handling

--- src/main/java/dev/stocky37/xiv/actions/core/RotationService.java
+++ src/main/java/dev/stocky37/xiv/actions/core/RotationService.java
@@ -2,19 +2,18 @@ package dev.stocky37.xiv.actions.core;
 
 import dev.stocky37.xiv.actions.config.XivConfig;
 import dev.stocky37.xiv.actions.data.Action;
-import dev.stocky37.xiv.actions.data.Consumable;
+import dev.stocky37.xiv.actions.data.Delay;
 import dev.stocky37.xiv.actions.data.Item;
 import dev.stocky37.xiv.actions.data.Rotation;
 import dev.stocky37.xiv.actions.data.RotationInput;
 import dev.stocky37.xiv.actions.json.RotationBuilder;
-import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
 
 @ApplicationScoped
 public class RotationService {
-	private final AbilityService actionService;
+	private final AbilityService abilityService;
 	private final ItemService itemService;
 	private final XivConfig config;
 
@@ -24,26 +23,37 @@ public class RotationService {
 		ItemService itemService,
 		XivConfig config
 	) {
-		this.actionService = abilityService;
+		this.abilityService = abilityService;
 		this.itemService = itemService;
 		this.config = config;
 	}
 
 	public Rotation buildRotation(RotationInput input) {
-		final List<Action> actions = input.actions().stream()
-			.map(id -> {
-				if(!id.startsWith("i")) {
-					return actionService.findById(id).orElseThrow();
-				}
-				final var item = itemService.findById(id.substring(1)).orElseThrow();
-				if(item.kind() != Item.Kind.CONSUMABLE) {
-					throw new BadRequestException("Unsupported item kind: " + item.kind().toString());
-				}
-				return (Action)item;
-			})
-			.toList();
+		final var builder = new RotationBuilder(config);
+		input.actions().forEach(str -> builder.append(parseAction(str)));
+		return builder.build();
+	}
 
-		return new RotationBuilder(config).withActions(actions).build();
+	private Action parseAction(String str) {
+		// standard delay for nulls for now
+		if(str == null) {
+			return new Delay(config.animationLock());
+		}
+
+		// starts with i, then it's an item
+		if(str.startsWith("i")) {
+			final var id = str.substring(1);
+			final var item = itemService.findById(id)
+				.orElseThrow(() -> new BadRequestException("No item found with id: " + id));
+			if(item.kind() != Item.Kind.CONSUMABLE) {
+				throw new BadRequestException("Unsupported item kind: " + item.kind().toString());
+			}
+			return (Action) item;
+		}
+
+		// otherwise, it's an ability
+		return abilityService.findById(str)
+			.orElseThrow(() -> new BadRequestException("No ability found with id: " + str));
 	}
 }
 
