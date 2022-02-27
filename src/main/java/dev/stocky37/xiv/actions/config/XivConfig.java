c6a5ba5f2740865134e501063dbf67e6de7e1505
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-02-27 15:47:31 +0000

    refactor: give actions animationLock value
    - Also stopped them extending ApiObject

---
+++ src/main/java/dev/stocky37/xiv/actions/config/XivConfig.java
@@ -0,0 +1,12 @@
+package dev.stocky37.xiv.actions.config;
+
+import io.smallrye.config.ConfigMapping;
+import java.time.Duration;
+
+@ConfigMapping(prefix = "xiv")
+public interface XivConfig {
+	Duration ping();
+	Duration animationLock();
+	Duration baseGcd();
+	int gcdGroup();
+}
